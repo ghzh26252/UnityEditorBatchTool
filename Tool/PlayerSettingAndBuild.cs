eeb1d627003c9d6bbb842a18d23b06188256f895
zyb <wrzh26252@outlook.com>
2020-05-26 16:13:28 +0800

    Update PlayerSettingAndBuild.cs
    隐藏场景完整路径，只保留场景名。
    存储方式改为GUID，关卡数量和顺序发生变更后不影响未变化的场景。

--- Tool/PlayerSettingAndBuild.cs
+++ Tool/PlayerSettingAndBuild.cs
@@ -9,66 +9,103 @@ public class PlayerSettingAndBuild : EditorWindow
         GetWindow<PlayerSettingAndBuild>("多场景一键发布");
     }
 
-    List<OneBuild> builds = new List<OneBuild>();
+    List<FrountBuild> frountBuilds = new List<FrountBuild>();
+    List<BackBuild> backBuilds = new List<BackBuild>();
     string buildPath = "E:/Desktop";
     Vector2 vector2;
-    class OneBuild
+    class FrountBuild
     {
         public string name;
-        public List<bool> scenesBool;
-        public OneBuild()
+        public List<bool> scenesBool = new List<bool>();
+        public FrountBuild()
         {
-            name = "";
-            scenesBool = new List<bool>();
             for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
             {
                 scenesBool.Add(false);
             }
         }
     }
+    class BackBuild
+    {
+        public string name;
+        public List<string> scenesGUID = new List<string>();
+    }
+    List<FrountBuild> Back2Frount(List<BackBuild> backBuildsT)
+    {
+        List<FrountBuild> fbs = new List<FrountBuild>();
+        foreach(BackBuild backBuildT in backBuildsT)
+        {
+            FrountBuild fb = new FrountBuild();
+            fb.name = backBuildT.name;
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+            {
+                if (backBuildT.scenesGUID.Contains(EditorBuildSettings.scenes[i].guid.ToString()))
+                    fb.scenesBool[i] = true;
+            }
+            fbs.Add(fb);
+        }
+        return fbs;
+    }
+    List<BackBuild> Frount2Back(List<FrountBuild> frountBuildsT)
+    {
+        List<BackBuild> bbs = new List<BackBuild>();
+        foreach (FrountBuild frountBuildT in frountBuildsT)
+        {
+            BackBuild bb = new BackBuild();
+            bb.name = frountBuildT.name;
+            for (int i=0;i< frountBuildT.scenesBool.Count;i++)
+            {
+                if (frountBuildT.scenesBool[i])
+                    bb.scenesGUID.Add(EditorBuildSettings.scenes[i].guid.ToString());
+            }
+            bbs.Add(bb);
+        }
+        return bbs;
+    }
 
     void OnGUI()
     {
         vector2 = GUILayout.BeginScrollView(vector2);
-        for (int k = 0;k < builds.Count; k++)
+        for (int k = 0;k < frountBuilds.Count; k++)
         {
             GUILayout.BeginHorizontal();
-            builds[k].name = EditorGUILayout.TextField("发布名称", builds[k].name);
+            frountBuilds[k].name = EditorGUILayout.TextField("发布名称", frountBuilds[k].name);
             GUILayout.BeginVertical();
             for(int i = 0; i<EditorBuildSettings.scenes.Length; i++)
             {
-                builds[k].scenesBool[i] = GUILayout.Toggle(builds[k].scenesBool[i], EditorBuildSettings.scenes[i].path);
+                frountBuilds[k].scenesBool[i] = GUILayout.Toggle(frountBuilds[k].scenesBool[i], EditorBuildSettings.scenes[i].path.Split('/', '.')[EditorBuildSettings.scenes[i].path.Split('/','.').Length-2]);
             }
             GUILayout.EndVertical();
             if (GUILayout.Button("-", GUILayout.Width(30)))
             {
-                builds.RemoveAt(k);
+                frountBuilds.RemoveAt(k);
             }
             GUILayout.EndHorizontal();
             GUILayout.Label("----------------");
         }
         if (GUILayout.Button("+"))
         {
-            builds.Add(new OneBuild());
+            frountBuilds.Add(new FrountBuild());
         }
         GUILayout.EndScrollView();
 
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("清空关卡"))
         {
-            builds.Clear();
+            frountBuilds.Clear();
         }
-            if (GUILayout.Button("保存配置方案"))
+        if (GUILayout.Button("保存配置方案"))
         {
-            if (builds.Count <= 0)
+            if (frountBuilds.Count <= 0)
             {
                 Debug.Log("请添加发布场景");
                 return;
             }
+            backBuilds = Frount2Back(frountBuilds);
             List<string> builds2Json = new List<string>();
-            foreach (OneBuild onebuild in builds)
+            foreach (BackBuild backBuildT in backBuilds)
             {
-                builds2Json.Add(JsonUtility.ToJson(onebuild));
+                builds2Json.Add(JsonUtility.ToJson(backBuildT));
             }
             System.IO.File.WriteAllLines(Application.dataPath + "/PlayerSettingAndBuild.txt", builds2Json);
         }
@@ -77,11 +114,12 @@ public class PlayerSettingAndBuild : EditorWindow
             if (System.IO.File.Exists(Application.dataPath + "/PlayerSettingAndBuild.txt"))
             {
                 string[] Json2builds = System.IO.File.ReadAllLines(Application.dataPath + "/PlayerSettingAndBuild.txt");
-                builds.Clear();
+                backBuilds.Clear();
                 foreach (string buildsJson in Json2builds)
                 {
-                    builds.Add(JsonUtility.FromJson<OneBuild>(buildsJson));
+                    backBuilds.Add(JsonUtility.FromJson<BackBuild>(buildsJson));
                 }
+                frountBuilds = Back2Frount(backBuilds);
             }
             else
                 Debug.Log("未找到配置方案");
@@ -99,12 +137,12 @@ public class PlayerSettingAndBuild : EditorWindow
         GUILayout.EndHorizontal();
         if (GUILayout.Button("开始发布"))
         {
-            if (builds.Count<=0)
+            if (frountBuilds.Count<=0)
             {
                 Debug.Log("请添加发布场景");
                 return;
             }
-            foreach(OneBuild oneBuild in builds)
+            foreach(FrountBuild oneBuild in frountBuilds)
             {
                 if (oneBuild.name == "")
                 {
@@ -117,21 +155,19 @@ public class PlayerSettingAndBuild : EditorWindow
                 Debug.Log("请选择发布场景路径");
                 return;
             }
-            foreach (OneBuild oneBuild in builds)
+            foreach (FrountBuild frountBuildT in frountBuilds)
             {
                 List<string> editorBuildSettingsScenes = new List<string>();
                 for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
                 {
-                    if (oneBuild.scenesBool[i])
+                    if (frountBuildT.scenesBool[i])
                     {
                         editorBuildSettingsScenes.Add(EditorBuildSettings.scenes[i].path);
                         Debug.Log(EditorBuildSettings.scenes[i].path);
                     }
                 }
-                PlayerSettings.productName = oneBuild.name;
-                //if (!System.IO.Directory.Exists(buildPath + "/" + oneBuild.name))
-                   // System.IO.Directory.CreateDirectory(buildPath + "/" + oneBuild.name);
-                BuildPipeline.BuildPlayer(editorBuildSettingsScenes.ToArray(), buildPath + "/" + oneBuild.name  + "/" + oneBuild.name + ".exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+                PlayerSettings.productName = frountBuildT.name;
+                BuildPipeline.BuildPlayer(editorBuildSettingsScenes.ToArray(), buildPath + "/" + frountBuildT.name  + "/" + frountBuildT.name + ".exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
             }
         }
         GUILayout.Space(5);
